e82e79e223041b9eee8263762f296689c54f348e
Gentlekuma <kumanoyuushi@gmail.com>
2015-10-30 15:52:49 +0100

    v 0.1

--- a_star_quad_tree/node.hpp
+++ a_star_quad_tree/node.hpp
@@ -1,13 +1,13 @@
 // Node.hpp
 
-
+// Class for quadtree used with A* algorithm
 
 #ifndef NODE
 #define NODE
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 #include <vector>
 #include <functional>
 struct PQCompareNode : public std::binary_function<Node*, Node*, bool>
@@ -22,26 +22,24 @@ enum Node_type {blocked, mixed, free, goal};
 
 class Node
 {
-	
     protected:
 	//Attributs from the map :
 	int x;
 	int y;
-	int resolution; //Largeur du node (1,2,4,8,16,32,...)
+	int resolution; //node size in pixels (1,2,4,8,16,32,...)
 	Node parent_node;
 	Node_type type;
-	std::array<Node*, 4> sons; //liste des sous-arbres
-	std::array<Node *, 4> neighborhood; //liste des voisins
+	std::array<Node*, 4> sons; //subtrees list
+	std::array<Node *, 4> neighborhood; //adjacent nodes list (4 nodes N,W,S,E) 
 	
 	//Attributs for A* w/ quad trees :
 	int weight_so_far;
 	Node coming_from;
 	int weight;
 
-	
     public:
-	Node(Node_type type, Vector<Node> neighborhood, Vector<Node> sons){
-	    this->weigh_so_far=MAXINT;
+	Node(int x, int y, int resolution, Node parent_node, Node_type type, std::array<Node*, 4> sons, std::array<Node* ,4> neighborhood){
+	    this->weigh_so_far=INT_MAX;
 	    this->parent_node = NULL;
 	    this->type = type;
 	    this->neighborhood = neighborhood;
@@ -59,5 +57,5 @@ class Node
 	
 	
 #endif
-
+	//use direction for cost (cost higher if changing direction and for quad trees)
 	//for latter : priority_queue<Node*, vector<Node*>, DereferenceCompareNode> queue
