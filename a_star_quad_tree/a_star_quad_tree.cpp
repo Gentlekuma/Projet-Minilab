f4347b4ad3b87de78066ca42ee0bf4850b2c9d24
Gentlekuma <kumanoyuushi@gmail.com>
2015-11-04 11:50:23 +0100

    v0.4

--- a_star_quad_tree/a_star_quad_tree.cpp
+++ a_star_quad_tree/a_star_quad_tree.cpp
@@ -1,23 +1,68 @@
-#include "node.hpp"
-
 #ifndef A_STAR_QT
 #define A_STAR_QT
 
-std::List<Node *> a_star_qt(Node *beginning, Node *goal){
-    
-    std::priority_queue<Node*, vector<Node*>, DereferenceCompareNode> frontier;
-    std::List<Node *> path;
-    Direction *d;
-    
-    exploreBeginning(beginning, frontier, path, &d);
+#include <queue>
+#include <vector>
 
-    //use direction for cost (cost higher if changing direction and for quad trees) with Dx and D
+#include "node.hpp"
+
+class Compare{
+    public:
+	bool operator()(const Node* lhs, const Node* rhs){
+	    return lhs->weight_so_far > rhs->weight_so_far;
+	}
+};
+
+
+std::list<Node *> a_star_qt(Node *beginning, Node *goal){
+    std::priority_queue<Node*, vector<Node*>, Compare> frontier;
+    std::List<Node *> path;
+    Direction d;
+    Direction new_d;
+    Node * frontier_top;
+    int new_weigh;
+
+    //Explore the first node :
+
+    beginning->weight_so_far=beginning->resolution;
     
-    while(!frontier.isEmpty() && !(frontier.top().isGoal()) ){ /
+    for (std::list<Node *>::iterator it=beginning->neighborhood.begin(); it != beginning->neighborhood.end(); ++it){
+	new_d = new_direction( it, frontier_top);
+	it->weight_so_far = beginning->weight_so_far + it->resolution + goal.heuristique( it );
+	it->coming_from = beginning;
+	it->previous_d = new_d;
+	
+	frontier.push(it);
+    }
+    
+    while(!frontier.isEmpty() && !(frontier.top().isGoal()) ){ 
 	//while frontier not empty and goal not found
 
-	exploreFrontier(frontier, path, &d);	
+	frontier_top = frontier.top();
+	frontier.pop();
 	
+	for (std::list<Node *>::iterator it=frontier_top->neighborhood.begin(); it != frontier_top->neighborhood.end(); ++it){
+	    new_d = new_direction( (*it), frontier_top);
+	    new_weigh = frontier_top->weight_so_far + (*it)->resolution + + goal.heuristique( (*it) ) + malus(new_d, frontier_top->previous_d);
+	    
+	    if(new_weigh < (*it)->weight_so_far){
+		(*it)->weight_so_far = new_weigh;
+		(*it)->coming_from = frontier_top;
+		(*it)->previous_d = new_d;
+		
+		frontier.push(*it);
+	    }
+	}
+    }
+    
+    // get the path recursively :
+    
+    if (!frontier.std::top().isGoal()){
+	Node temp = frontier.top();
+	while ( temp != NULL){
+	    path.push(temp);
+	    temp = temp->coming_from;
+	}
     }
     
     if (std::end(path).isGoal() ){
@@ -27,47 +72,6 @@ std::List<Node *> a_star_qt(Node *beginning, Node *goal){
     else{
 	std::cerr << "ZE PATH IS NOT FINISHED SCHIESSE" << std::endl;
 	exit(-1);
-    }
-}
-
-void exploreBeginning(Node *beginning, Node *goal, std::priority_queue<Node*, vector<Node*>, DereferenceCompareNode> frontier, std::list<Node *> path, Direction *d){
-    //Maj du cout
-    //Maj de frontier en rajoutant les voisins (changer leurs attributs)
-
-    Direction new_d;
-    
-    beginning->weight_so_far=beginning->resolution;
-    
-    for (std::list<Node *>::iterator it=beginning->neighborhood.begin(); it != beginning->neighborhood.end(); ++it){
-	new_d = new_direction( (*it), frontier_top);
-
-	(*it)->weight_so_far=beginning->weight_so_far + (*it)->resolution + goal.heuristique( (*it) );
-	(*it)->coming_from = beginning;
-	(*it)->previous_d = new_d;
-	
-	frontier.push(*it);
-    }
-}
-
-void exploreFrontier(Node *beginning, Node *goal, std::priority_queue<Node*, vector<Node*>, DereferenceCompareNode> frontier, std::list<Node *> path){
-    //Maj du cout (cost so far + heuristique + malus si changement de d)
-    //Maj du frontier + pop top si poids plus petit
-
-    Node * frontier_top = frontier.pop();
-    int new_weigh;
-    Direction new_d;
-    
-    for (std::list<Node *>::iterator it=frontier_top->neighborhood.begin(); it != frontier_top->neighborhood.end(); ++it){
-	new_d = new_direction( (*it), frontier_top);
-	new_weigh = frontier_top->weight_so_far + (*it)->resolution + + goal.heuristique( (*it) ) + malus(new_d, frontier_top->previous_d);
-
-	if(new_weigh < (*it)->weight_so_far){
-	    (*it)->weight_so_far = new_weigh;
-	    (*it)->coming_from = frontier_top;
-	    (*it)->previous_d = new_d;
-
-	    frontier.push(*it);
-	}
     }
 }
 
@@ -82,7 +86,7 @@ int malus(Direction new_d, Direction d){
 Direction new_direction(Node *from, Node *to){
     int delta_x = to->x - from->x;
     int delta_y = to->y - from->y;
-
+    
     if (abs(delta_x) > abs(delta_y)){
 	if (delta_x >=0){
 	    return DIR_E;
@@ -94,12 +98,12 @@ Direction new_direction(Node *from, Node *to){
     else{
 	if (delta_y >=0){
 	    return DIR_S
-	}
+		}
 	else{
 	    return DIR_N;
 	}
     }
 }
-		
+
 		      
 #endif
