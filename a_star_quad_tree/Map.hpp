fa177a669d07456443c7b98c2f61ab04d4c88ddb
Nlandais974 <nitsumo@hotmail.fr>
2015-11-11 02:41:07 +0100

    ajout Map.hpp

---
+++ a_star_quad_tree/Map.hpp
@@ -0,0 +1,14 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+
+/** Function Headers */
+void Erosion( int, void*);
+void Dilation( int, void*);
+
+void Fermeture(int, void*);
+void Ouverture(int, void*);
